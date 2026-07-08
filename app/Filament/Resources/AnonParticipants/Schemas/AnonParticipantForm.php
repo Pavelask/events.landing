fe5171949c917e8ad3c74bf2c73630b0cb87c0b4
Pavelask <pavelask@mail.ru>
2026-07-08 14:04:40 +0300

    fix: Section import path for Filament v5

--- app/Filament/Resources/AnonParticipants/Schemas/AnonParticipantForm.php
+++ app/Filament/Resources/AnonParticipants/Schemas/AnonParticipantForm.php
@@ -3,11 +3,10 @@
 namespace App\Filament\Resources\AnonParticipants\Schemas;
 
 use Filament\Forms\Components\DateTimePicker;
-use Filament\Forms\Components\KeyValue;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\Select;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Schema;
 
